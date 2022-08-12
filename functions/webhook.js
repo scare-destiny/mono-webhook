0906c074982c1031348f81b59dcc092ce9d47158
scare-destiny <zhenya.venger@gmail.com>
2022-08-12 17:39:47 +0300

    Send data to Apps Script for further processing

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,8 +1,24 @@
 exports.handler = async function (event, context) {
   console.log(event);
+  const body = JSON.parse(event.body);
+
+  let response = await fetch(
+    "https://script.google.com/macros/s/AKfycbziPb_3irULA1RlHD3p5A33EFKcvGY7Ec8wKtqcnbj7IJsAFasickXhta7J2Ed8mPIa-A/exec",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  let result = await response.json();
+  console.log(result.message);
 
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
   };
 };
+
