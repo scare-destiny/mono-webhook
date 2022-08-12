e8dc2568aa46c0ad9b037d09924d9048a61b1973
scare-destiny <zhenya.venger@gmail.com>
2022-08-12 17:45:05 +0300

    Switched to axios from fetch

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,9 +1,10 @@
+const axios = require("axios").default;
+
 exports.handler = async function (event, context) {
-  console.log(event);
   const body = JSON.parse(event.body);
 
-  let response = await fetch(
-    "https://script.google.com/macros/s/AKfycbziPb_3irULA1RlHD3p5A33EFKcvGY7Ec8wKtqcnbj7IJsAFasickXhta7J2Ed8mPIa-A/exec",
+  let response = await axios.post(
+    `https://script.google.com/macros/s/AKfycbziPb_3irULA1RlHD3p5A33EFKcvGY7Ec8wKtqcnbj7IJsAFasickXhta7J2Ed8mPIa-A/exec`,
     {
       method: "POST",
       headers: {
@@ -21,4 +22,3 @@ exports.handler = async function (event, context) {
     body: JSON.stringify({ message: "Hello World" }),
   };
 };
-
