e792bcadbd807917c24563f689c3dc57b831af13
scare-destiny <zhenya.venger@gmail.com>
2022-08-13 12:37:05 +0300

    Commented post request

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,19 +1,19 @@
 const axios = require("axios").default;
-
+// todo: revert to previos version
 exports.handler = async function (event, context) {
   const body = JSON.parse(event.body);
 
-  let response = await axios.post(
-    `https://script.google.com/macros/s/AKfycbziPb_3irULA1RlHD3p5A33EFKcvGY7Ec8wKtqcnbj7IJsAFasickXhta7J2Ed8mPIa-A/exec`,
-    {
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      test: 'test'
-    }
-  );
+  // let response = await axios.post(
+  //   `https://script.google.com/macros/s/AKfycbziPb_3irULA1RlHD3p5A33EFKcvGY7Ec8wKtqcnbj7IJsAFasickXhta7J2Ed8mPIa-A/exec`,
+  //   {
+  //     headers: {
+  //       "Content-Type": "application/json;charset=utf-8",
+  //     },
+  //     test: "test",
+  //   }
+  // );
 
-  let result = await response.json();
+  // let result = await response.json();
   console.log(result.message);
 
   return {
